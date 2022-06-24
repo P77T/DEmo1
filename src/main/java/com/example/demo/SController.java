89da8e5d429f902675dcaaaf2fac0cc3797b0e57
R <R>
2022-06-24 12:58:25 +0530

    Second commit

--- src/main/java/com/example/demo/SController.java
+++ src/main/java/com/example/demo/SController.java
@@ -1,5 +1,19 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class SController {
 
+    @GetMapping("/get")
+    public String getData(){
+        User u, u1;
+        u = new User(); 
+        u.setId("1");
+        u.setName("name");
+        
+        return u.getId() + " Name is " + u.getName();
+    }
 }
